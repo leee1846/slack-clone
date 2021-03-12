0cf037ee7a2c61a4b0377feef0527c78532f2525
leee1846 <leee1846@gmail.com>
2021-03-12 16:42:16 +0900

    style: 회원가입 스타일 적용

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+
+const Login = loadable(() => import('../pages/Login'));
+const SignUp = loadable(() => import('../pages/SignUp'));
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Router>
+      <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;

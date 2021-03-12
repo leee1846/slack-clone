dbd789221608dec73f6b0754ecc4759a84a8f575
leee1846 <leee1846@gmail.com>
2021-03-13 00:13:07 +0900

    feature: login api 요청

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '../SignUp/style';
+import useInput from './../../hooks/useInput';
+import axios from 'axios';
+import useSwr from 'swr';
+import fetcher from './../../utils/fetcher';
 
 const Login = () => {
-  return <div>로그인</div>;
+  const { data, error, revalidate } = useSwr('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval = 2000,
+  });
+
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const [loginError, setLoginError] = useState(false);
+
+  const onSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      setLoginError(false);
+      axios
+        .post(
+          'http://localhost:3095/api/users/login',
+          {
+            email,
+            password,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then((response) => {
+          revalidate();
+          console.log(response);
+        })
+        .catch((error) => {
+          setLoginError(error.response?.data?.status === 401);
+        });
+    },
+    [email, password],
+  );
+
+  return (
+    <div id="email-label">
+      <Header>Select</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        {loginError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default Login;

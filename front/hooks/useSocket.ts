0ee358a8efc22c057a64454740067d60f61609c8
leee1846 <leee1846@gmail.com>
2021-03-24 14:45:02 +0900

    fix: useSocket type fix

--- front/hooks/useSocket.ts
+++ front/hooks/useSocket.ts
@@ -6,7 +6,7 @@ const backUrl = `http://localhost:3095`;
 
 const sockets: { [key: string]: SocketIOClient.Socket } = {};
 
-const useSocket = (workspace: string) => {
+const useSocket = (workspace?: string): [SocketIOClient.Socket | undefined, () => void] => {
   const disconnect = useCallback(() => {
     if (workspace) {
       sockets[workspace].disconnect();

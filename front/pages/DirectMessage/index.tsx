919d97c0513c37a741815cba7623bcd83719417a
leee1846 <leee1846@gmail.com>
2021-03-24 13:17:39 +0900

    feature: DM보내기 적용
    - error: 500에러 뜸

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -7,19 +7,36 @@ import { useParams } from 'react-router-dom';
 import ChatBox from './../../components/ChatBox/index';
 import ChatList from './../../components/ChatList/index';
 import useInput from './../../hooks/useInput';
+import axios from 'axios';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
-
   const { data: userData } = useSwr(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSwr(`/api/users`, fetcher);
+  const { data: chatData, mutate: mutateChat, revalidate } = useSwr(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
+
   const [chat, onChangeChat, setChat] = useInput('');
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-    setChat('');
-    console.log('submit');
-  }, []);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!chat.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            setChat('');
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
